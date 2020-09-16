3cf6d429cb436d5d3cad82f6725addcdbd74bb7b
Hugo Duncan <hugo@hugoduncan.org>
2020-09-16 06:16:27 -0400

    Update for makejack 0.0.1-alpha1

--- Formula/makejack.rb
+++ Formula/makejack.rb
@@ -2,14 +2,14 @@ class Makejack < Formula
 
   desc "A Clojure build tool invoker."
   homepage "https://github.com/hugoduncan/makejack"
-  version "0.0.1-alpha1-SNAPSHOT"
+  version "0.0.1-alpha1"
 
   if OS.linux?
-    url "https://github.com/hugoduncan/makejack/releases/download/v0.0.1-alpha1-SNAPSHOT/makejack-0.0.1-alpha1-SNAPSHOT-linux-amd64.zip"
-    sha256 "18c4d358a06266212fa10e55eadcf4d2d23958313e44c04e37bfdc89d4022379"
+    url "https://github.com/hugoduncan/makejack/releases/download/v0.0.1-alpha1/makejack-0.0.1-alpha1-linux-amd64.zip"
+    sha256 "d6d00459629bc5affa24ef219b108359a82993883f2f740f1cd0f08c567ed3af"
   else
-    url "https://github.com/hugoduncan/makejack/releases/download/v0.0.1-alpha1-SNAPSHOT/makejack-0.0.1-alpha1-SNAPSHOT-macos-amd64.zip"
-    sha256 "7aaa49955b3a96587f87c002251a4dec94ad58323476afc94c29ebfe39ce9406"
+    url "https://github.com/hugoduncan/makejack/releases/download/v0.0.1-alpha1/makejack-0.0.1-alpha1-macos-amd64.zip"
+    sha256 "f195978dfa5b722b5f66a2bd9a1012d663bc926e9f8c8128ae7dfde7e39eb01b"
   end
 
   bottle :unneeded
